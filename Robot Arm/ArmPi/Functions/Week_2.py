8c6294dead6b855b1813ac753c4188de1c5dac99
aivilo-d <olivia.dawes2@gmail.com>
2026-02-19 12:33:11 -0800

    perception code

---
+++ Robot Arm/ArmPi/Functions/Week_2.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+# coding=utf8
+import sys
+sys.path.append('/home/pi/ArmPi/')
+import cv2
+import time
+import Camera
+import math
+import numpy as np
+from ArmIK.Transform import *
+
+class Tracker():
+    
+    def __init__(self, camera, target_color, size = (640, 480)):
+        self.target_color = target_color
+        self.camera = camera
+        self.size = size
+        self.square_length = 3
+        self.detected_color = None
+        self.color_range = {
+        'red': [(0, 151, 100), (255, 255, 255)], 
+        'green': [(0, 0, 0), (255, 115, 255)], 
+        'blue': [(0, 0, 0), (255, 255, 110)], 
+        'black': [(0, 0, 0), (56, 255, 255)], 
+        'white': [(193, 0, 0), (255, 250, 255)], 
+        }
+
+    def getAreaMaxContour(self, contours):
+        contour_area_temp = 0
+        contour_area_max = 0
+        area_max_contour = None
+        for c in contours:  # Traversing all contours
+            contour_area_temp = math.fabs(cv2.contourArea(c))  # Calculate contour area
+            if contour_area_temp > contour_area_max:
+                contour_area_max = contour_area_temp
+                if contour_area_temp > 300:  # The contour of the largest area is only effective when the area is greater than 300, in order to filter out interference
+                    area_max_contour = c
+        return area_max_contour, contour_area_max 
+
+    def get_image(self, img):
+        img_copy = img.copy()
+        img_h, img_w = img.shape[:2]
+        cv2.line(img, (0, int(img_h / 2)), (img_w, int(img_h / 2)), (0, 0, 200), 1)
+        cv2.line(img, (int(img_w / 2), 0), (int(img_w / 2), img_h), (0, 0, 200), 1)
+        frame_resize = cv2.resize(img_copy, self.size, interpolation=cv2.INTER_NEAREST)
+        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
+        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)
+        return frame_lab
+
+    def find_contours(self, frame_lab):
+        for i in self.color_range:
+            if i in self.target_color:
+                self.detected_color = i
+                frame_mask = cv2.inRange(frame_lab, self.color_range[self.detected_color][0], self.color_range[self.detected_color][1])  #perform bitwise operations on original image and mask
+                opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  #opening operation
+                closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  #closing operation
+                contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  #find the outline
+                areaMaxContour, area_max = self.getAreaMaxContour(contours) 
+                return areaMaxContour, area_max
+    
+    def find_block(self, areaMaxContour, area_max):
+        if area_max > 2500:  #the largest area was found
+            rect = cv2.minAreaRect(areaMaxContour)
+            box = np.int0(cv2.boxPoints(rect))
+
+            roi = getROI(box) #get ROI region
+
+            img_centerx, img_centery = getCenter(rect, roi, self.size, self.square_length)  #get center coordinates of the wooden block
+            
+            world_x, world_y = convertCoordinate(img_centerx, img_centery, self.size) #convert to real world coordinates
+            
+            
+            cv2.drawContours(img, [box], -1, self.color_range[self.detected_color], 2)
+            cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.color_range[self.detected_color], 1) #draw the center point
+
+    def track(self, img):
+        frame_lab = self.get_image(img)
+        areaMaxContour, area_max = self.find_contours(frame_lab)
+        self.find_block(areaMaxContour, area_max)
+
+if __name__ == '__main__':
+    my_camera = Camera.Camera()
+    my_camera.camera_open()
+    tracker = Tracker(my_camera, target_color)
+    while True:
+        img = tracker.camera.frame
+        if img is not None:
+            frame = img.copy()
+            Frame = tracker.track(frame)           
+            cv2.imshow('Frame', Frame)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+    my_camera.camera_close()
+    cv2.destroyAllWindows()
+    
